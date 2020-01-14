bf13b51a373b57080ba37236e35ec65cb68cef19
BeHustle <46607810+BeHustle@users.noreply.github.com>
2020-01-14 15:33:02 +0300

    Завершает стилизацию по стайлгайду

--- source/js/script.js
+++ source/js/script.js
@@ -5,11 +5,20 @@ $('.travel-filter__slider').slider({
   values: [30,100],
   range: true,
   stop: function(event, ui) {
-    $(".travel-filter__level-number--min").val($(".travel-filter__slider").slider("values",0));
-    $(".travel-filter__level-number--max").val($(".travel-filter__slider").slider("values",1));
+    $('.travel-filter__level-number--min').val($('.travel-filter__slider').slider('values',0));
+    $('.travel-filter__level-number--max').val($('.travel-filter__slider').slider('values',1));
   },
   slide: function(event, ui){
     $(".travel-filter__level-number--min").val($(".travel-filter__slider").slider("values",0));
     $(".travel-filter__level-number--max").val($(".travel-filter__slider").slider("values",1));
   }
+});
+
+$('.page-header').addClass('page-header--js')
+$('.page-header__toggle-menu').click(function () {
+  $('.page-header--js').toggleClass('page-header--opened')
+});
+
+$('.travel-filter__show-btn').click(function () {
+  $(this).parent().toggleClass('travel-filter__fieldset--opened')
 });
